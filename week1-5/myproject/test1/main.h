3b54b0fb63f8729fc2ecca9425cb7bc77521dbb3
1411552145@qq.com <1411552145@qq.com>
2019-10-22 00:35:26 -0700

    week7

--- week1-5/myproject/test1/main.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <sys/time.h>
-#include <unistd.h>
-
-long get_qsort_time(int *);
-long get_my_time(int *);
-int compar(const void *a,const void *b);
