3938f3eb6b4a2598f4cc64df8efd2a52992b346b
1411552145@qq.com <1411552145@qq.com>
2019-10-22 00:16:40 -0700

    week7

---
+++ week7/code/hellow.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(int argc,char *argv[])
+{
+	//int i;										//(1)
+	//for(i=0;i<205;i++)
+	//{
+		printf("hello");
+	//}
+	
+	//fclose(stdout);								//(2)	
+
+	//setvbuf(stdout,NULL,_IONBF,0);				//(3)
+	while(1);
+}
---
+++ week7/code/openfopen.cpp
@@ -0,0 +1,38 @@
+#include<unistd.h>
+#include<iostream.h>
+#include<fcntl.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+using namespace std;
+
+int main(){
+	int fd;
+	FILE *file;
+	char *s="hello,world\n";
+	if((fd=open("test.txt",O_WRONLY|O_CREAT,S_IRUS|S_IWUSR)) == -1){
+		cout<<"Error Open file"<<endl;
+		return -1;
+	}
+	
+	if((fd=open("test2.txt","w")) == NULL){
+		cout<<"Error Open file"<<endl;
+		return -1;
+	}
+	cout<<"File has been Opened"<<endl;	
+	sleep(15);
+	if(write(fd,s,strlen(s)) < strlen(s)){
+		cout<<"Write Error"<<endl;
+		return -1;
+	}
+	if(fwrite(s,strlen(char),strlen(s),file) < strlen(s)){
+		cout<<"Write Error in 2"<<endl;
+		return -1;
+	}
+	cout<<"After write"<<endl;
+	sleep(15);
+	cout<<"After sleep"<<endl;
+	close(fd);
+	return 0;		
+}
+
