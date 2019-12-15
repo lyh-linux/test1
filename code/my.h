8116acc5729b6213867111c973ceadfaf0789eb0
1411552145@qq.com <1411552145@qq.com>
2019-12-15 03:29:53 -0800

    week14

---
+++ code/my.h
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<wait.h>
+#include<signal.h>
+#include<time.h>
+#include<sys/param.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<string.h>
+#include<errno.h>
+#include<fcntl.h>
+#include<sys/wait.h>
+#include<setjmp.h>
+#include<fcntl.h>
+#include<pthread.h>
